140d03d7c8f694204a64c28eacbc7db5b8b0ab03
bettermu <1113783908@qq.com>
2018-07-17 22:44:26 +0800

    完善头部页面

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
 }
 </style>
--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>这是头部</div>
+  <div class="m-header">
+    <h1 class="text">网易云音乐</h1>
+    <router-link to="/user" class="mine" tag="div">
+      <i class="iconfont icon-list"></i>
+    </router-link>
+    <router-link to="/search" class="search" tag="div">
+      <i class="iconfont icon-search"></i>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -7,3 +15,7 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
