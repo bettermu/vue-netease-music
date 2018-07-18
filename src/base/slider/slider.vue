fcff30287231af13f2a774cd5bf982cc754d2e4a
bettermu <1113783908@qq.com>
2018-07-18 16:53:28 +0800

    添加swiper组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="../../common/image/default.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="../../common/image/default.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="../../common/image/default.jpg" alt="">
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+export default {
+  mounted () {
+    const mySwiper = new Swiper('.swiper-container', {
+      autoplay: true,
+      loop: true
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import 'swiper/dist/css/swiper.min.css';
+</style>
+
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <div>我是recommend</div>
+    <slider></slider>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
+import Slider from 'base/slider/slider'
 
+export default {
+  components: {
+    Slider
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
