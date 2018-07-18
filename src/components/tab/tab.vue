09b87a4cbfcd498c3e00c5074e1b56f53cc1ac36
bettermu <1113783908@qq.com>
2018-07-18 11:09:45 +0800

    调整公用样式的px单位，编写tab组件样式和结构

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>我是tab</div>
+  <div class="tab">
+    <router-link tag="div" class="tab-item" to="/recommend">
+      <span>推荐</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/rank">
+      <span>排行</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/singer">
+      <span>歌手</span>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -9,5 +19,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "common/scss/variable.scss";
+.tab {
+  display:flex;
+  line-height:1.375rem;
+  height:1.375rem;
+  width:100%;
+  font-size:$font-size-medium;
+  background: $color-theme;
+  .tab-item {
+    flex:1;
+    text-align:center;
+    color:$color-text-lm;
+    span {
+      padding-bottom:.09375rem
+    }
+    &.router-link-active {
+      span {
+        font-weight:bold;
+        color:$color-text-l;
+        border-bottom:.0625rem solid $color-text-l;
+      }
+    }
+  }
+}
 
 </style>
