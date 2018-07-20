78e703259d14620a817aafd8a7005c8b0bf75bda
bettermu <1113783908@qq.com>
2018-07-20 10:20:35 +0800

    推荐歌单列表数据对接

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,1 +1,4 @@
-export const HOST='http://localhost:3000'
+export const HOST = 'http://localhost:3000'
+
+// 成功码
+export const ERR_OK = 200
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -4,17 +4,17 @@
     <div class="recommend-list">
       <h1 class="title">推荐歌单</h1>
       <ul>
-        <li class="item">
+        <li class="item" v-for="item in playList" :key="item.id">
           <div class="icon">
             <div class="gradients"></div>
-            <img src="../../common/image/default.jpg" alt="">
+            <img v-lazy="item.picUrl">
           </div>
           <p class="play-count">
             <i class="fa fa-headphones"></i>
-            10000万
+            {{Math.floor(item.playCount / 10000 )}}万
           </p>
           <div class="text">
-            <p class="name">我是哈哈</p>
+            <p class="name">{{item.name}}</p>
           </div>
         </li>
       </ul>
@@ -25,23 +25,41 @@
 
 <script>
 import Slider from 'base/slider/slider'
-import { getBanner } from 'api/recommend'
+import { getBanner, getRecommendList } from 'api/recommend'
+import { ERR_OK } from 'common/js/config'
 
 export default {
   data() {
     return {
-      banner: []
+      banner: [],
+      playList: [],
     }
   },
   created() {
     this._getBanner()
+    this._getRecommendList()
   },
   methods: {
     _getBanner() {
       getBanner().then((res) => {
-        this.banner = res.data.banners
+        if (res.status === ERR_OK) {
+          this.banner = res.data.banners
+        } else {
+          console.error('banner获取失败')
+        }
+
       })
-    }
+    },
+    _getRecommendList() {
+      getRecommendList().then(res => {
+        if (res.status === ERR_OK) {
+          console.log(res.data)
+          this.playList = res.data.result
+        } else {
+          console.error('getRecommend获取失败')
+        }
+      })
+    },
   },
   components: {
     Slider
@@ -53,66 +71,74 @@ export default {
 @import "~common/scss/variable";
 @import "~common/scss/mixin";
 .recommend {
-  position:fixed;
-  width:100%;
-  top:2.75rem;
-  bottom:0;
-  z-index:100;
+  position: fixed;
+  width: 100%;
+  top: 2.75rem;
+  bottom: 0;
+  z-index: 100;
   .recommend-list {
-    position:relative;
-    box-sizing:border-box;
-    width:100%;
-    text-align:center;
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;
+    text-align: center;
     .title {
-      height:2.03125rem;
-      line-height:2.03125rem;
-      padding-left:1.5%;
-      text-align:left;
-      font-size:$font-size-medium;
-      font-weight:bold;
-      color:$color-text;
+      height: 2.03125rem;
+      line-height: 2.03125rem;
+      padding-left: 1.5%;
+      text-align: left;
+      font-size: $font-size-medium;
+      font-weight: bold;
+      color: $color-text;
     }
     .item {
-      display:inline-block;
-      position:relative;
+      display: inline-block;
+      position: relative;
       box-sizing: border-box;
-      width:33%;
-      padding:0 1%;
+      width: 33%;
+      padding: 0 1%;
       .icon {
-        position:relative;
-        display:inline-block;
-        width:100%;
-        margin-bottom:.15625rem;
+        position: relative;
+        display: inline-block;
+        width: 100%;
+        margin-bottom: .15625rem;
         .gradients {
-          position:absolute;
-          top:0;
-          width:100%;
-          height:1.09375rem;
-          border-radius:.09375rem;
-          background:linear-gradient(rgba(109,109,109,.4),rgba(255,255,255,0));
+          position: absolute;
+          top: 0;
+          width: 100%;
+          height: 1.09375rem;
+          border-radius: .09375rem;
+          background: linear-gradient(rgba(109, 109, 109, .4), rgba(255, 255, 255, 0));
         }
         img {
-          width:100%;
-          height:100%;
+          width: 100%;
+          height: 100%;
           border-radius: .09375rem;
         }
       }
       .play-count {
-        position:absolute;
+        position: absolute;
         top: .15625rem;
-        right:.25rem;
-        font-size:$font-size-small-s;
-        color:$color-text-l;
+        right: .25rem;
+        font-size: $font-size-small-s;
+        color: $color-text-l;
       }
       .text {
-        float:left;
-        line-height:.5rem;
-        text-align:left;
-        height:1.25rem;
-        line-height:.5rem;
-        overflow:hidden;
-        margin-bottom:10px;
-        font-size:$font-size-small;
+        float: left;
+        line-height: .5rem;
+        text-align: left;
+        line-height: .5rem;
+        overflow: hidden;
+        margin-bottom: 10px;
+        font-size: $font-size-small;
+
+        .name {
+          width: 100%;
+          overflow: hidden;
+          /*text-overflow: ellipsis;  有些示例里需要定义该属性，实际可省略*/
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
       }
     }
   }
