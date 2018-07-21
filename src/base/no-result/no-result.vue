6bb81b30eeb4d16b42b41853e5e79e31ae641995
bettermu <1113783908@qq.com>
2018-07-21 22:37:27 +0800

    编写无结果组件

---
+++ src/base/no-result/no-result.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="no-result">
+    <div class="no-result-icon">
+      <i class="fa fa-music"></i>
+    </div>
+    <p class="no-result-text">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "~common/scss/variable";
+@import "~common/scss/mixin";
+
+.no-result {
+  text-align:center;
+  .no-result-icon {
+    color:$color-text-gg;
+    font-size:1.5625rem;
+  }
+  .no-result-text {
+    margin-top:.9375rem;
+    font-size:$font-size-medium;
+    color:$color-text;
+  }
+}
+
+</style>
+
