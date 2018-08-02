4170a4e61959b88801c755cafc717ec4bd892bc6
bettermu <1113783908@qq.com>
2018-08-02 22:55:08 +0800

    排行TOP列表数据测试

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -6,7 +6,32 @@
 </template>
 
 <script>
-export default {
+import { getTop } from "api/rank";
+import Scroll from "base/scroll/scroll";
+import Loading from "base/loading/loading";
+import { mapMutations } from "vuex";
 
-}
+//获取TOP列表id参数数组
+const YUNMUSIC_TOP = [0, 1, 2, 3, 4, 22, 23];
+export default {
+  data() {
+    return {
+      yunTopList: [],
+      showLoading: true
+    };
+  },
+  created() {
+    this._getTopList();
+  },
+  methods: {
+    //获取top列表
+    _getTopList() {
+      for (let i = 0; i < YUNMUSIC_TOP.length; i++) {
+        getTop(YUNMUSIC_TOP[i]).then(res => {
+          console.log(res);
+        });
+      }
+    }
+  }
+};
 </script>
