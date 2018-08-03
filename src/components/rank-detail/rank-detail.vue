67a9363fdfd7f073116e6ef1c236fe015682c1aa
bettermu <1113783908@qq.com>
2018-08-03 22:50:38 +0800

    rank详情页调试

--- src/components/rank-detail/rank-detail.vue
+++ src/components/rank-detail/rank-detail.vue
@@ -1,6 +1,6 @@
 <template>
-  <transition name="slide" mode="out-in">
-    <div>rank详情页{{$route.params.id}}</div>
+  <transition name="slide">
+    <div class="music-list">rank详情页{{$route.params.id}}</div>
   </transition>
 
 </template>
@@ -10,3 +10,27 @@ export default {
 
 }
 </script>
+
+<style scoped lang="scss">
+@import "~common/scss/variable";
+@import "~common/scss/mixin";
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.2s
+}
+.slide-enter, .slide-leave-to {
+  transform: translate3d(30%, 0, 0);
+  opacity: 0;
+}
+
+.music-list {
+  position:fixed;
+  z-index:100;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background: $color-background;
+}
+
+</style>
+
