173ca4e31279bb432ca116f4ad5584fd61272691
bettermu <1113783908@qq.com>
2018-08-03 22:19:26 +0800

    rank样式编写

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -4,7 +4,7 @@
       <ul>
         <li class="item" v-for="item in yunTopList" :key="item.id">
           <div class="icon">
-            <img v-lazy="item.coverImgUrl" width="100" height="100">
+            <img v-lazy="item.coverImgUrl">
           </div>
           <ul class="songlist">
             <li class="song" v-for="(song,index) in item.top" :key="index">
@@ -45,7 +45,7 @@ export default {
     _getTopList() {
       for (let i = 0; i < YUNMUSIC_TOP.length; i++) {
         getTop(YUNMUSIC_TOP[i]).then(res => {
-          console.log(res)
+          console.log(res);
           let list = res.data.playlist;
           //只取前三
           list.top = res.data.playlist.tracks.slice(0, 3);
@@ -72,6 +72,61 @@ export default {
 .rank {
   position: fixed;
   width: 100%;
+  top: 2.75rem;
+  bottom: 0;
+  .toplist {
+    height: 100%;
+    overflow: hidden;
+    padding-top: 0.15625rem;
+    .item {
+      display: flex;
+      margin: 0 0.3125rem;
+      padding: 0.09375rem 0;
+      height: 3.125rem;
+      border-bottom: 0.03125rem solid rgb(228, 228, 228);
+      &:last-child {
+        padding-bottom: 0.625rem;
+      }
+      .icon {
+        flex: 0 0 3.125rem;
+        width: 3.125rem;
+        height: 3.125rem;
+        img {
+          height: 3.125rem;
+          width: 3.125rem;
+          border-radius: 0.09375rem;
+        }
+      }
+      .songlist {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 0.625rem;
+        height: 3.125rem;
+        overflow: hidden;
+        // background: $color-dialog-background;
+        color: $color-text;
+        font-size: $font-size-small-x;
+        .song {
+          @include no-wrap();
+          line-height: 0.9375rem;
+        }
+      }
+    }
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+  .loading-content {
+    position: fixed;
+    width: 100%;
+    top: 70%;
+    transform: translateY(-50%);
+  }
 }
 </style>
 
