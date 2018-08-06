2242e8d693ae002c9f39c848ed5a864adb6987f6
bettermu <1113783908@qq.com>
2018-08-06 22:24:49 +0800

    rank-detail页面结构样式编写

--- src/components/rank-detail/rank-detail.vue
+++ src/components/rank-detail/rank-detail.vue
@@ -1,6 +1,37 @@
 <template>
   <transition name="slide">
-    <div class="music-list">rank详情页{{$route.params.id}}</div>
+    <div class="music-list">
+      <div class="header" ref="header">
+        <div class="back">
+          <i class="fa fa-angle-left"></i>
+        </div>
+        <div class="text">
+          <h1 class="title">{{headerTitle}}</h1>
+        </div>
+      </div>
+      <div class="music-list-wrapper">
+        <div class="bg-image" :style="bgStyle" ref="bgImage">
+          <div class="filter"></div>
+          <div class="text">
+            <h2 class="list-title">
+              {{title}}
+            </h2>
+            <p class="update">{{updateTime}}</p>
+          </div>
+        </div>
+        <div class="song-list-wrapper">
+          <div class="sequence-play" v-show="listDetail.length">
+            <i class="iconfont icon-bofangicon"></i>
+            <span class="text">播放全部</span>
+            <span class="count">(共{{listDetail.length}}首)</span>
+          </div>
+          <song-list :songs="listDetail"></song-list>
+        </div>
+      </div>
+      <div v-show="!listDetail.length" class="loading-content">
+        <loading></loading>
+      </div>
+    </div>
   </transition>
 
 </template>
@@ -28,9 +59,9 @@ export default {
       this.$router.push("/rank");
     }
 
-    this._normalizeSongs(this.topList.tracks)
-    this.probeType = 3
-    this.listenScroll = true
+    this._normalizeSongs(this.topList.tracks);
+    this.probeType = 3;
+    this.listenScroll = true;
   },
 
   computed: {
@@ -44,11 +75,11 @@ export default {
         return;
       }
       let ret = [];
-      list.forEach((item) => {
+      list.forEach(item => {
         ret.push(createRecommendListSong(item));
       });
       this.listDetail = ret;
-      console.log(ret)
+      console.log(ret);
     }
   }
 };
@@ -75,6 +106,112 @@ export default {
   right: 0;
   bottom: 0;
   background: $color-background;
+  .header {
+    position:fixed;
+    top:0;
+    width:100%;
+    height:1.375rem;
+    color:#fff;
+    z-index:100;
+    .back {
+      position:absolute;
+      top:0;
+      left:.125rem;
+      .fa-angle-left {
+        padding:.15625rem .3125rem;
+        font-size:.9375rem;
+      }
+    }
+    .text {
+      position:absolute;
+      left:1.1875rem;
+      line-height:1.375rem;
+      font-size: $font-size-medium-x;
+      @include no-wrap()
+    }
+  }
+  .music-list-wrapper {
+    .bg-image {
+      position:relative;
+      width:100%;
+      height:0;
+      padding-top:75%;
+      transform-origin: top;
+      background-size: cover;
+      background-position: 0 30%;
+      .filter {
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background:#fff;
+        opacity: 0.2;
+      }
+      .text {
+        position:absolute;
+        width:80%;
+        height:1.25rem;
+        bottom:1.5625rem;
+        left:.625rem;
+        color:#fff;
+        .list-title {
+          position:absolute;
+          bottom: 0;
+          font-style:italic;
+          font-size:$font-size-large;
+          line-height:.5625rem;
+          font-weight:bold;
+          letter-spacing: .03125rem;
+          .music {
+            position:absolute;
+            top:-0.625rem;
+            left:.15625rem;
+            font-style: italic;
+            font-weight: bold;
+            font-size: $font-size-medium;
+          }
+        }
+        .update {
+          position:absolute;
+          top:1.40625rem;
+          left:.21875rem;
+          line-height:.4375rem;
+          font-size: $font-size-small;
+        }
+      }
+    }
+    .song-list-wrapper {
+      padding:1.28125rem 0 .625rem 0;
+      border-radius:.3125rem;
+      position:relative;
+      top:-0.625rem;
+      background: $color-background;
+      .sequence-play {
+          position: absolute;
+          // left: 8;
+          top: 0px;
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 1.25rem;
+          padding-left: .5rem;
+          border-bottom: .03125rem solid rgb(228, 228, 228);
+          .iconfont {
+            font-size: .5625rem;
+            color: $color-text;
+            padding-right: .4375rem;
+          }
+          .text {
+            font-size: $font-size-medium-x;
+          }
+          .count {
+            font-size: $font-size-medium;
+            color: $color-text-g;
+          }
+        }
+    }
+  }
 }
 </style>
 
